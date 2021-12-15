d3c28c3d09edc507061093aa44337888ee7b1aa8
Mickael Daniel <daniel.mickael@gmail.com>
2021-12-15 18:06:43 +0100

    Tweak styles / theme

---
+++ src/@rocketseat/gatsby-theme-docs/styles/theme.js
@@ -0,0 +1,20 @@
+export default {
+  colors: {
+    // like active (for links hover and such)
+    primary: '#8257E6',
+    background: '#121212',
+    // used for active link background in sidebar, and th / tds
+    shape: `#121212`,
+    title: `#fff`,
+    text: `#b7bcbe`,
+    components: {
+      blockquote: {
+        background: `#feebc8`,
+        text: `#2d3748`,
+      },
+    },
+  },
+  fonts: {
+    body: `-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif`,
+  },
+};
