87f110154389de21c27a43932b6b7d6b7efc85a4
Mickael Daniel <daniel.mickael@gmail.com>
2021-12-16 17:41:09 +0100

    Setup sidebars and general pages structure

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,11 @@
 module.exports = {
   siteMetadata: {
-    siteTitle: `Rocket Docs`,
-    defaultTitle: `Rocket Docs`,
-    siteTitleShort: `Rocket Docs`,
-    siteDescription: `Out of the box Gatsby Theme for creating documentation websites easily and quickly`,
-    siteUrl: `https://rocketdocs.netlify.app`,
-    siteAuthor: `@jpedroschmitz`,
+    siteTitle: `Combo Graph`,
+    defaultTitle: `Combo Graph`,
+    siteTitleShort: `Combo Graph`,
+    siteDescription: `Combo Graph is an Unreal plugin based on GAS (Gameplay Ability System) that lets you focus on Combo / Combat design with a Custom Graph and Editor to deliver a fast, flexible and enjoyable developer experience. The main idea behind this plugin is to try to cut down iteration times as much as possible when designing and prototyping a new combat system / combos.`,
+    siteUrl: `https://combograph.netlify.app`,
+    siteAuthor: `@mklabs`,
     siteImage: `/banner.png`,
     siteLanguage: `en`,
     themeColor: `#8257E6`,
@@ -18,15 +18,16 @@ module.exports = {
       options: {
         configPath: `src/config`,
         docsPath: `src/docs`,
-        repositoryUrl: `https://github.com/jpedroschmitz/rocketdocs`,
-        baseDir: `examples/gatsby-theme-docs`,
+        branch: `master`,
+        repositoryUrl: `https://github.com/mklabs/ComboGraphDocs`,
+        baseDir: ``,
       },
     },
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Rocket Docs`,
-        short_name: `Rocket Docs`,
+        name: `Combo Graph`,
+        short_name: `Combo Graph`,
         start_url: `/`,
         background_color: `#ffffff`,
         display: `standalone`,
